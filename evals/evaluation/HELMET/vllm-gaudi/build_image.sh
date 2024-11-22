18e243417757073cc53524266b3091dfb15d6cf0
minmin-intel <minmin.hou@intel.com>
2024-11-22 13:41:09 -0800

    add vllm gaudi
    Signed-off-by: minmin-intel <minmin.hou@intel.com>

---
+++ evals/evaluation/HELMET/vllm-gaudi/build_image.sh
@@ -0,0 +1,22 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+echo "Building the vllm-gaudi docker images"
+cd $WORKDIR
+echo $WORKDIR
+if [ ! -d "./vllm" ]; then
+    git clone https://github.com/HabanaAI/vllm-fork.git
+fi
+cd ./vllm-fork
+git checkout habana_main
+
+docker build -f Dockerfile.hpu -t vllm-gaudi:latest --shm-size=128g . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
+if [ $? -ne 0 ]; then
+    echo "vllm-gaudi:latest failed"
+    exit 1
+else
+    echo "vllm-gaudi:latest successful"
+fi
+
+
