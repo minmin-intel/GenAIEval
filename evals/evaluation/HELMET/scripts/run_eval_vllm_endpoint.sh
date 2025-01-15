3e3c25980f263c1e722be4084fe946f08a25981f
minmin-intel <minmin.hou@intel.com>
2025-01-15 22:57:07 +0000

    add ut test yamls

--- evals/evaluation/HELMET/scripts/run_eval_vllm_endpoint.sh
+++ evals/evaluation/HELMET/scripts/run_eval_vllm_endpoint.sh
@@ -2,5 +2,6 @@
 port=8085 # change it to the actual port that vllm container is running at
 llm_endpoint_url="http://${host_ip}:${port}/v1"
 echo "llm_endpoint_url is $llm_endpoint_url"
+config_file=configs/rag_test/rag_short_test_8k.yaml
 
-python3 eval.py --config configs/rag_short_test.yaml --endpoint_url $llm_endpoint_url
+python3 eval.py --config $config_file --endpoint_url $llm_endpoint_url
