1d9467f2d75eb256e4a4ff6ae9f54b020232ffd3
minmin-intel <minmin.hou@intel.com>
2025-01-16 21:48:26 +0000

    add more reproduce test yamls

--- evals/evaluation/HELMET/scripts/run_eval_vllm_endpoint.sh
+++ evals/evaluation/HELMET/scripts/run_eval_vllm_endpoint.sh
@@ -2,6 +2,6 @@
 port=8085 # change it to the actual port that vllm container is running at
 llm_endpoint_url="http://${host_ip}:${port}/v1"
 echo "llm_endpoint_url is $llm_endpoint_url"
-config_file=configs/rag_test/rag_short_test_8k.yaml
+config_file=configs/reproduce_tests/recall_1B.yaml
 
 python3 eval.py --config $config_file --endpoint_url $llm_endpoint_url
